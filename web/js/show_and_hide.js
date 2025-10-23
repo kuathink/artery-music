9e07a4ea306609e36c0cdf54f2dcd74486c2ba86
oaken <2532615203@qq.com>
2025-10-23 14:17:35 +0000

    删除文件 web

--- web/js/show_and_hide.js
+++
@@ -1,8 +0,0 @@
-function display(id) {
-    var traget = document.getElementById(id);
-    if (traget.style.display == "none") {
-        traget.style.display = "";
-    } else {
-        traget.style.display = "none";
-    }
-}
